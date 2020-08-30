f80823e6489fd94b455d512bcb8b869d304f4f24
mix64 <mic@mix64.com>
2020-08-30 18:16:13 +0900

    fix freeze_graph.sh bugs

--- neural_network/freeze_graph.sh
+++ neural_network/freeze_graph.sh
@@ -2,11 +2,11 @@
 
 echo "usage: ./freeze_graph MODEL_DIR FREEZED_NAME"
 
-MODEL_DIR=$0
-FREEZED_NAME=$1
+MODEL_DIR=$1
+FREEZED_NAME=$2
 
-freeze_graph --input_meta_graph $MODELDIR/checkpoints/model.meta
-             --output_graph FREEZED_NAME
-             --output_node_names Embedding1/dense/BiasAdd
-             --input_bin
+freeze_graph --input_meta_graph $MODEL_DIR/checkpoints/model.meta \
+             --output_graph $FREEZED_NAME \
+             --output_node_names Embedding1/dense/BiasAdd \
+             --input_bin \
              --input_checkpoint $MODEL_DIR/checkpoints/model
